cd4871cdf1348874af84332f8ef652434b97e54f
Onkabetse Sebothoma <gosiameonkabetse077@gmail.com>
2025-09-20 18:08:11 +0200

    Working on the portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,10 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 
 export const routes: Routes = [
-  { path: '', component: Home }
+  { path: '', component: Home },
+  //{ path: 'portfolio', component: Portfolio },
+  //{ path: 'about', component: AboutComponent },
+  //{ path: 'contact', component: ContactComponent },
+  //{ path: 'admin', component: AdminDashboardComponent },
+  //{ path: '**', redirectTo: '', pathMatch: 'full' }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navigation } from "./navigation/navigation";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navigation],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('OnePhoto');
+  isAdminMode = false;
+
+  setIsAdminMode(value: boolean) {
+    this.isAdminMode = value;
+  }
 }
